c164ac56c329f6624cd10b43ee3c9a13d5ae8070
bing <TG8623@protonmail.com>
2024-02-21 22:12:37 +0800

    add keep

---
+++ script/JS/keep.js
@@ -0,0 +1,19 @@
+/*
+ * https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/keep.js
+
+*******************************
+
+[Script]
+keep = type=http-response, requires-body=true, max-size=-1, timeout=60, pattern=^https:\/\/api\.gotokeep\.com, script-path=https://raw.githubusercontent.com/freeman963/ios_script/main/script/JS/keep.js
+
+[MITM]
+hostname = %APPEND% api.gotokeep.com
+
+*******************************
+
+*/
+
+body = $response['body']['replace'](/\"memberStatus":\d+/g, '"memberStatus":1')['replace'](/\"buttonText":".*?"/g, '"buttonText":"已永久"')['replace'](/\"hasPaid\":\w+/g, '"hasPaid":true')['replace'](/\"downLoadAll\":\w+/g, '"downLoadAll":true')['replace'](/\"videoTime\":\d+/g, '"videoTime":3000')['replace'](/\"startEnable\":\w+/g, '"startEnable":true')['replace'](/\"memberStatus\":\d+/g, '"memberStatus":1')['replace'](/\"preview\":\w+/g, '"preview":true')['replace'](/\"errorCode\":\d+/g, '"errorCode":0')['replace'](/\"status\":\w+/g, '"status":true');
+var _0x1337d8 = {};
+_0x1337d8['body'] = body;
+$done(_0x1337d8);
